7d8984d41f5b0f868c2b6e1bcb40ed4bf62735b7
onotaizee@gmail.com <onotaizee@gmail.com>
2023-04-19 14:27:04 +0100

    +added: improved email verification content

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -23,6 +23,26 @@ export function sendVerificationMail(receiver, verificationLink) {
 	
 				<div style='padding: 3px 8px; text-align: center;'>
 					<h2>Hey there new user 👋, welcome to WhatScheduler</h2>
+
+					<h3>What is WhatScheduler?</h3>
+					<pre>
+						Whatscheduler is an Open Source project that allows you to schedule WhatsApp messages to be sent at a later time.
+						Currently, it also supports sending messages to multiple contacts at once as well as sending recurring messages.
+					</pre>
+					<br/>
+
+					<h3>Why do I need to verify my email?</h3>
+					<pre>
+						We need to verify your email to ensure that you are a real person and not a bot.
+						We also need to verify your email to ensure that you are the owner of the WhatsApp account you are using.
+						We do not store your WhatsApp credentials, we only use them to send messages to your contacts.
+					</pre>
+
+					<h3>How can I contribute to this project?</h3>
+					<pre>
+						You can contribute to this project by reporting bugs, suggesting new features, or even by contributing code.
+						You can find the source code for this project on <a href="https://github.com/DreadedHippy/WhatScheduler">GitHub</a>.
+					</pre>
 					<p style="text-align: center">Click the link below to verify your email</p>
 					<br/>
 	
